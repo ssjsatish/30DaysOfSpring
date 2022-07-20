e4064b4b58dd66abdd94394805db8951fc41595c
satish patel <ssj.satish@gmail.com>
2022-07-21 00:28:34 +0530

    additional commit

--- src/main/java/com/tcm/hello/HelloApp.java
+++ src/main/java/com/tcm/hello/HelloApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApp {
     public static void main(String[] args) {
         SpringApplication.run(HelloApp.class, args);
-        System.out.println("Hello World!");
+        System.out.println("Hello World, Satish!");
     }
 }
